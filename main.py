6d16e3858f47035a1ea648fcb5b32b0c4ab30370
Alexandros Giannakopoulos <alexgiannakopoulos@hotmail.com>
2025-04-16 13:09:32 +0300

    refactoring main with bot events
    replaced client events with bot events to create and modify bot commands and functionality

--- main.py
+++ main.py
@@ -1,44 +1,49 @@
 # bot.py
 import os
 import random
-
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')      #use secrets from .env
+
+load_dotenv()                           #loads the .env file and gets the values below
+TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-intents = discord.Intents.default()
-intents.members = True
+intents = discord.Intents.default()     #initializes the intents used by the bot in any server
+intents.members = True                  #something like permissions in discord
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(command_prefix='!',intents=intents)      #sets the prefix for bot commands, can be set to almost anything
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.event
+async def on_member_join(member):                #when a new member joins sends a dm welcoming them
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+@bot.event
 async def on_ready():
     
-    guild = discord.utils.get(client.guilds, name=GUILD)        #loop through data discord sent about guilds
+    guild = discord.utils.get(bot.guilds, name=GUILD)        #loop through data discord sent about guilds
 
     print(
-        f'{client.user} has connected to the following Guild:\n'
+        f'{bot.user} has connected to the following Guild:\n'
         f'{guild.name}(id: {guild.id})'
         )
     
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
-    )
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
+@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+async def nine_nine(ctx):
+    
     brooklyn_99_quotes = [
         'I\'m the human form of the 💯 emoji.',
         'Bingpot!',
@@ -48,21 +53,30 @@ async def on_message(message):
         ),
     ]
 
-    if message.content == '99!':
-        response = random.choice(brooklyn_99_quotes)
-        await message.channel.send(response)
-    elif message.content == 'raise-exception':
-        raise discord.DiscordException
-    
-    if 'happy birthday' in message.content.lower():
-        await message.channel.send('Happy Birthday! 🎈🎉')
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
 
-@client.event
-async def on_error(event, *args, **kwargs):
-    with open('err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
-        else:
-            raise
+@bot.command(name='roll_dice', help='Simulates rolling dice and posts the result. type the command followed by the number of dice and the number of sides ex: !roll_dice 2 6')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
 
-client.run(TOKEN)
+@bot.command(name='create-channel')
+@commands.has_role('admin')
+async def create_channel(ctx, channel_name='real-python'):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+
+
+bot.run(TOKEN)
